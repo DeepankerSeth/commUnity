e9eafafbb7db23109fd757093aa36b3b37779e93
sethdeepanker0 <sethdeepanker0@gmail.com>
2024-09-12 12:32:32 -0700

    tried fixing axios bug again

--- backend-nodejs/src/routes/disasterPrediction.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/predict', (req, res) => {
-  res.json({ message: 'Disaster prediction endpoint' });
-});
-
-module.exports = router;
--- backend-nodejs/src/utils/data cleaning.js
+++
@@ -1,7 +0,0 @@
-// src/utils/dataCleaning.js
-const cleanData = (data) => {
-    // Implement data cleaning logic
-    return data;
-  };
-  
-  module.exports = { cleanData };
